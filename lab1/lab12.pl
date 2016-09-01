a6489124fe7cc9f60c1c84824de614d117ce527f
sciion <martin.estgren@gmail.com>
2016-09-01 17:20:17 +0200

    fixed lab 1.2

--- lab1/lab12.pl
+++ lab1/lab12.pl
@@ -1,11 +1,3 @@
-node(a).
-node(b).
-node(c).
-node(d).
-node(e).
-node(f).
-node(g).
-node(h).
 edge(a,b).
 edge(a,c).
 edge(b,c).
@@ -20,13 +12,19 @@ app([],Ys,Ys).
 app([X|Xs],Ys, [X|Zs]) :-
 	app(Xs, Ys, Zs).
 path(X,Y) :-
-	edge(X,Y).
+	X == Y.
 path(X,Y) :-
 	edge(X,Z),
 	path(Z,Y).
+%path/3
 path(X,Y,Z) :-
-	edge(X,Y).
+	X == Y,
+	app([X],[],Z).
 path(X,Y,Z) :-
 	edge(X,N),
-	path(N,Y,W).
-
+	path(N,Y,W),
+	app([X],W,Z).
+%pathn/3
+pathn(X,Y,Z) :-
+	path(X,Y,W),
+	length(W,Z).
