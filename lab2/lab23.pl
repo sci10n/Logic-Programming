2bd4d9f8c0fc6164ccaa0b3f7f364b7123e21e57
sciion <martin.estgren@gmail.com>
2016-09-27 22:14:48 +0200

    added test cases for lab2.2 and 2.3

--- lab2/lab23.pl
+++ lab2/lab23.pl
@@ -1,3 +1,35 @@
+%Inorder-insert element in list
+insert(X,[],[X]).
+insert(X,[A|Y],[X|[A|Y]]) :-
+	X =< A.
+insert(X,[A|Y],[A|W]) :-
+	X > A,
+	insert(X,Y,W).
+
+%Insertion sort. 
+isort([],[]).
+isort([A|X],Y) :-
+	isort(X,Z),
+	insert(A,Z,Y).
+
+%Pivot clauses used by the quick-sort algorithm
+pivot(_,[],L,G,L,G).
+
+pivot(P,[X|Y],L,G,Lr,Gr) :-
+    X =<P,
+    pivot(P,Y,[X|L],G, Lr, Gr).
+pivot(P,[X|Y],L,G, Lr,Gr) :-
+    X > P,
+    pivot(P,Y,L,[X|G],Lr,Gr).
+        
+%quick-sort clauses. 
+qsort([],[]).
+qsort([P|Li],Lo) :-
+	pivot(P,Li,[],[],Lr,Gr),
+	qsort(Lr,QLr),
+	qsort(Gr,QGr),
+	append(QLr,[P|QGr],Lo).
+
 %Define number and identifier literals
 num(N).
 id(I).
@@ -113,3 +145,26 @@ powerset([],[[]]).
 powerset(A,C) :-
 	set(A),
 	setof(X,(member(X,A), subseq0(A,X)),C).
+
+% -------------------------
+% ------EXAMPLE QUERY------
+% -------------------------
+%
+% Sort the list [2,1,4,3,5] using isort
+% -----------------------------------------
+%
+% ?- isort([2,1,4,3,5],X).
+% X = [1,2,3,4,5]
+%
+% Sort the list [2,1,4,3,5] using qsort
+% ------------------------------------
+%
+% ?- qsort([2,1,4,3,5],X).
+% X = [1,2,3,4,5] 
+%
+% Execute the program  seq(set(id(a),num(1)), set(id(a),id(a)+num(2))
+% ---------------------------------------------
+%
+% ?- command([],seq(set(id(a),num(1)), set(id(a),id(a)+num(2))),X).
+% X = [set(a, 3)] ;
+% no
