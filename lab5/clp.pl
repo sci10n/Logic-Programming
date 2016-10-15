22ea3de926c81674c1ad8790338e85df452d0c25
Martin Estgren <martin.estgren@gmail.com>
2016-10-15 11:45:09 +0200

    worked on lab5

--- lab5/clp.pl
+++ lab5/clp.pl
@@ -7,14 +7,42 @@ container(d,1,1).
 
 on(a,d).
 on(b,c).
-on(c.d).
+on(c,d).
 
-task(List) :-
-%    Tasks = [task(_,2,_,2),
-%             task(_4,_,1),
-%             task(_,2,_2),
-%             task(_,1,_,1)] 
-    maplist(is_top,Tasks,List).
-    
-is_top(C) :-
-    on(C,_).
+schedule(Sum) :-
+ 	on_to_list(OnList),
+	container_to_list(ContainerList),
+ 	length(ContainerList,N),
+ 	length(StartList,N),
+ 	length(EndList,N),
+ 	domain(StartList, 0, 100),
+    domain(EndList, 0, 100),
+ 	task(StartList,EndList, ContainerList, Tasks),
+ 	WorkersRequired in 1..100,
+ 	cumulative(Tasks,[limit(WorkersRequired)]),
+ 	max_end_time(EndList,MaxEndTime),
+ 	Sum #= WorkersRequired * MaxEndTime.
+
+max_end_time([],_).
+max_end_time([End|EndList],MaxEndTime) :-
+	MaxEndTime #>= End,
+	max_end_time(EndList,MaxEndTime).
+
+container_to_list(List) :- findall([Id,Workers,Duration], container(Id,Workers,Duration),List).
+on_to_list(List) :- findall([X,Y],on(X,Y),List).
+
+on_top(X,[]).
+on_top(X,[[_,T]|List]) :-
+	dif(X,T),
+	on_top(X,List).
+
+constraint(Id,Workers,Time,ContraintList) :- 
+	on_top(Id,ContraintList).
+constraint(Id,Workers,Time) :- 
+	container(Id,RequiredWorkers, RequiredTime),
+	Workers #>= RequiredWorkers.
+
+task([],[],[],[]).
+task([Start|StartList], [End|EndList], [[Id,Workers,Duration]|ContainerList], [T|TaskList]) :-
+	T = task(Start,Duration,End,Workers,0),
+	task(StartList,EndList,ContainerList,TaskList).
